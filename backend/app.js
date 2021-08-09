2e4b57f7deaa80eee3312cde8314365cdddf8526
s21kaankoc <kaankoc@sabanciuniv.edu>
2021-08-09 16:42:47 +0300

    Docker Installation

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/kaan', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://mongo-db:27017/kaan', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
